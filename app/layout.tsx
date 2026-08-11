832a9ad7b465f0fcd478e0dfb5d50a3b93f080c7
FOOTYSPRO <footys1024@gmail.com>
2026-08-11 13:15:39 +0200

    Actualización rápida

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// 1. Importamos el componente Footer
+import Footer from "./components/Footer"; 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -36,7 +39,15 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-full flex flex-col">
-        {children}
+        
+        {/* Envolvemos children en flex-grow para empujar el Footer siempre abajo */}
+        <div className="flex-grow">
+          {children}
+        </div>
+        
+        {/* 2. Colocamos el Footer al final */}
+        <Footer />
+        
       </body>
     </html>
   );
